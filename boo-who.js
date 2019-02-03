ec5ae6c830bbca79bea981a8fb418b295cf5fec4
Brian Munoz <brianmunozdev@gmail.com>
2019-02-03 06:18:29 -0800

    boo who algorithm

---
+++ boo-who.js
@@ -0,0 +1,10 @@
+// https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-algorithm-scripting/boo-who
+
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if (typeof bool === "boolean") {
+    return true;
+  }
+  return false;
+}
+  
